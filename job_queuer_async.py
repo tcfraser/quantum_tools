a3b50cd666ff508d1422e88c7c51edd3dcff381b
TC Fraser <tcfraser237@gmail.com>
2016-05-27 12:15:34 -0400

    core request info

--- job_queuer_async.py
+++ job_queuer_async.py
@@ -7,16 +7,17 @@ from pprint import pprint
 class JobContext():
 
     def __init__(self, target_func, target_args, num_cores=-1, processes_per_core=1, log_worker=False):
-        self.num_evals = len(target_args)
         self.target_args = target_args
         self.target_func = target_func
+        self.processes_per_core = processes_per_core
         self.num_evals = len(target_args)
         if num_cores <= 0:
-            self.num_cores = multiprocessing.cpu_count() - 1
+            self.num_cores_requested = multiprocessing.cpu_count() - 1
         else:
-            self.num_cores = min(num_cores, multiprocessing.cpu_count())
-        print("JobContext using {0} cores.".format(self.num_cores))
-        self.processes_per_core = processes_per_core
+            self.num_cores_requested = min(num_cores, multiprocessing.cpu_count())
+        self.num_cores_needed = min(self.num_evals / self.processes_per_core, self.num_cores_requested)
+        print("JobContext requested {0} cores.".format(self.num_cores_requested))
+        print("JobContext using {0} cores.".format(self.num_cores_needed))
         self.log_worker = log_worker
         self.target_results = []
 
@@ -26,7 +27,7 @@ class JobContext():
 
     def evaluate(self):
         out_queue = Queue()
-        num_processes = self.num_cores * self.processes_per_core
+        num_processes = self.num_cores_needed * self.processes_per_core
         pool = Pool(processes = num_processes)
         for i in range(self.num_evals):
             pool.apply_async(self.target_func, args=self.target_args[i], callback=self._store_result)
