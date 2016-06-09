09356a60777dfff15dd893be1ad8f968f5665a36
TC Fraser <tcfraser237@gmail.com>
2016-06-09 14:00:22 -0400

    better pruning

--- code/quantum_tools/inflation/marginal_equality_prune.py
+++ code/quantum_tools/inflation/marginal_equality_prune.py
@@ -9,13 +9,13 @@ def pre_process(A, b):
     A_csr = A.tocsr()
     zero_b = np.where(b == 0.0)[0]
     A_row_zero_b = A_csr[zero_b, :]
-    col_to_delete = np.unique(A_row_zero_b.indices)
+    col_to_delete = A_row_zero_b.indices # Over counts
     rows_to_delete = zero_b
     rows_to_keep = integer_map.comp_mask(rows_to_delete, A.shape[0])
     col_to_keep = integer_map.comp_mask(col_to_delete, A.shape[1])
 
     A_row_removed = A_csr[rows_to_keep,:]
-    A_pruned = A_row_removed[:,col_to_keep]
+    A_pruned = A_row_removed.tocsc()[:,col_to_keep]
 
     b_pruned = np.delete(b, zero_b, axis=0)
 
